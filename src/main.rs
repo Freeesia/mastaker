ec8a33ce2fd06daa822ac6357f02f2feed0fc966
Freesia <freesia@studiofreesia.com>
2023-08-11 18:10:55 +0000

    ハッシュタグ投稿する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use reqwest;
 use rss::Channel;
 use serde_derive::{Deserialize, Serialize};
@@ -23,6 +24,7 @@ async fn fetch_rss(url: &str) -> Result<Channel, reqwest::Error> {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("local.yml")?;
     let config: Config = serde_yaml::from_reader(file)?;
+    let re = Regex::new(r"[^\w]").unwrap(); // 単語文字以外の文字にマッチする正規表現
     let client = megalodon::generator(
         megalodon::SNS::Mastodon,
         config.mastodon_url,
@@ -32,14 +34,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         let channel = fetch_rss(&config.rss_url).await?;
-        for item in channel.items().iter().take(1) {
-            // 例として最新の1つだけを取得
-            let content = format!(
-                "{} - {}",
-                item.title().unwrap_or(""),
-                item.link().unwrap_or("")
-            );
-            client.post_status(content, None).await?;
+        for item in channel.items().iter().skip(1).take(1) {
+            let mut content_parts = vec![
+                item.title().unwrap_or("").to_string(),
+                item.link().unwrap_or("").to_string(),
+                item.categories()
+                    .iter()
+                    .map(|c| format!("#{}", re.replace_all(&c.name(), "_")))
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            ];
+            // 空の文字列を削除
+            content_parts.retain(|part| !part.is_empty());    
+            client.post_status(content_parts.join("\n"), None).await?;
         }
         sleep(Duration::from_secs(3600)).await; // 1時間毎にチェック
     }
